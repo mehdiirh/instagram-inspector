c4331a523ddbc62d20f146a94740046fb40b4b65
Mehdi Rahimi <mehdii.rh78@gmail.com>
2024-04-08 00:16:19 +0330

    [ADD] inspectors stats

--- telegram/main.py
+++ telegram/main.py
@@ -88,7 +88,7 @@ async def inspector_stats(message: Message):
     text = f"**Users being inspected by** `{inspector.username}`**:**\n\n"
     for user in inspector_users:
         text += (
-            f"**Username:** [{user.username}](https://instagram.com/{user.username})"
+            f"**Username:** [{user.username}](https://instagram.com/{user.username})\n"
             f"**ID:** `{user.ig_pk}`\n"
             f"**Followers:** `{user.follower_count}`\n"
             f"**Followings:** `{user.following_count}`\n\n"

7777fb7cf020337c6f24a2776cc9127f53cc218b
Mehdi Rahimi <mehdii.rh78@gmail.com>
2024-04-07 21:48:44 +0330

    [UPDATE] create database if not exists

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os.path
+import shutil
 
 if __name__ == "__main__":
     if not os.path.exists("database.sqlite3"):
-        raise ValueError(
-            "Database not found. copy database-sample.sqlite3 and rename it to database.sqlite3"
-        )
+        shutil.copy("database-sample.sqlite3", "database.sqlite3")
 
     # importing telegram will execute program
     from telegram import main  # noqa
